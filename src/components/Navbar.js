c79b4ddd88d8272b3078aac7cdb72736f6d60efb
Deepesh <vermadeepesh248@gmail.com>
2025-02-14 23:48:51 +0530

    added cart, seller, help feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,13 @@ import order from "../resource/icon/orders-bfe8c4.svg";
 import wishlists from "../resource/icon/wishlist.svg";
 import rewards from "../resource/icon/rewards-fbd212.svg";
 import giftcard from "../resource/icon/giftCard-bd87e1.svg";
+import cart from "../resource/icon/header_cart-eed150.svg";
+import store from "../resource/icon/Store-9eeae2.svg";
+import dots from "../resource/icon/header_3verticalDots-ea7819.svg";
+import adverise from "../resource/icon/advertise-298691.svg";
+import notification from "../resource/icon/notificationPreferences-cfffaf.svg";
+import download from "../resource/icon/downloadApp-2ea657.svg";
+import helpcenter from "../resource/icon/helpcenter-7d90c0.svg";
 import React, { useState } from "react";
 
 function Navbar() {
@@ -30,6 +37,13 @@ function Navbar() {
     "Gift Cards",
   ]);
   const icons = [myProfile, fkplus, order, wishlists, rewards, giftcard];
+  const [help] = useState([
+    "Notificaton Preferences",
+    "24X7 Customer Care",
+    "Advertise",
+    "Download App",
+  ]);
+  const helpIcons = [notification, helpcenter, adverise,download]
   return (
     <>
       <div className={`${StyleNav.nav} ${StyleNav.cursor}`}>
@@ -113,7 +127,35 @@ function Navbar() {
             </div>
           </div>
         </div>
+
+        {/* cart section */}
+        <div className={`${StyleNav.align} ${StyleNav.cart}`}><a className={`${StyleNav.link} ${StyleNav.align}`} href="##"><img className={StyleNav.icon} src={cart} alt="cart" />Cart</a></div>
+     
+        {/* seller section */}
+        <div className={`${StyleNav.align} ${StyleNav.store}`}><a className={`${StyleNav.link} ${StyleNav.align}`} href="##"><img className={StyleNav.icon} src={store} alt="cart" />Become a Seller</a></div>
+
+        {/* help section */}
+        <div className={`${StyleNav.align} ${StyleNav.helpLink} ${StyleNav.alignV} ${StyleNav.dropdown}`}><img src={dots} className={`${StyleNav.helpDot} `} alt="help links" />
+        <div className={`${StyleNav.border} ${StyleNav.dropdowncontent} ${StyleNav.helpdropdown}`}>
+            <div className={`${StyleNav.alignV} `}>
+              {help.map((list, index) => (
+                <a
+                  href="##"
+                  className={`${StyleNav.loginDrop} ${StyleNav.btnSearchBar} ${StyleNav.helpDrop}  `}
+                >
+                  <img
+                    className={`${StyleNav.icon} ${StyleNav.align} `}
+                    src={helpIcons[index]}
+                    alt="help drop down"
+                  />
+                  {list}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
+      
     </>
   );
 }
