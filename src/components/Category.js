909d37c26dd19f3e73dc3d56bcdd66f4b7c94cce
Deepesh <vermadeepesh248@gmail.com>
2025-02-17 23:15:32 +0530

    fashion dropdown ongoing

--- src/components/Category.js
+++ src/components/Category.js
@@ -10,61 +10,90 @@ import toys from "../resource/category/dff3f7adcf3a90c6.png";
 import arrow from '../resource/icon/down-arrow.png'
 import style from '../css/category.module.css'
 
+
 function Category() {
   return (
     <>
       <div className={style.container}>
         <ul className={style.ul}>
-          <li>
-            <a href="##">
+          <li className={style.categoryList}>
+            <a className={style.category} href="##">
               <img src={kilos} alt="kilos" />
               Kilos
             </a>
           </li>
-          <li>
-            <a href="##">
+          <li className={style.categoryList}>
+            <a className={style.category} href="##">
               <img src={mobiles} alt="mobiles" />
               Mobiles
             </a>
           </li>
-          <li className={style.dropdown}>
-            <span>
+          <li className={`${style.dropdown} ${style.categoryList}`}>
+            <span className={style.category}>
               <img src={fashion} alt="fashion" />
               <div>Fashion&nbsp; <img className={style.icon} src={arrow} alt="arrow"/></div>
             </span>
+            <div className={style.dropdownContainer}>
+            <ul className={style.dropdownUl}> 
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Bottom Wear <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Top Wear <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men Footwear <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Women Footwear <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Watches and Accessories <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Women Western <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Bags, Suitcases & Luggage <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Kids <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Essentials <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Winter <img className={style.iconCat} src={arrow} alt="arrow"/></a></li>
+              </ul>
+              <ul className={`${style.dropdownUl} ${style.dropdownSubUl} ${style.fashion}`}> 
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">More in Men's Top Wear </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">All</a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's T-Shirts </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Casual Shirts </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Formal Shirt </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Kurtas</a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Ethnic Sets </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Blazers </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Raincoat </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Windcheaters </a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Suit</a></li>
+              <li className={style.dropdownList}><a className={style.dropdownCat} href="##">Men's Fabrics</a></li>
+            </ul>
+            </div>
           </li>
-          <li className={style.dropdown}>
-            <span>
+          <li className={`${style.dropdown} ${style.categoryList}`}>
+            <span className={style.category}>
               <img src={electronics} alt="electronics" />
               <div>Electronics&nbsp; <img className={style.icon} src={arrow} alt="arrow"/></div>
             </span>
           </li>
-          <li className={style.dropdown}>
-            <span>
+          <li className={`${style.dropdown} ${style.categoryList}`}>
+            <span className={style.category}>
               <img src={furniture} alt="home & furniture" />
               <div>Home & Furniture&nbsp; <img className={style.icon} src={arrow} alt="arrow"/></div>
             </span>
           </li>
-          <li>
-            <a href="##">
+          <li className={style.categoryList}>
+            <a className={style.category} href="##">
               <img src={appliances} alt="appliances" />
               Appliances
             </a>
           </li>
-          <li>
-            <a href="##">
+          <li className={style.categoryList}>
+            <a className={style.category} href="##">
               <img src={flight} alt="flight bookings" />
               Flight Bookings
             </a>
           </li>
-          <li className={style.dropdown}>
-            <span>
+          <li className={`${style.dropdown} ${style.categoryList}`}>
+            <span className={style.category}>
               <img src={toys} alt="beauty, toys & more" />
               <div>Beauty, Toys & More&nbsp; <img className={style.icon} src={arrow} alt="arrow"/></div>
             </span>
           </li>
-          <li className={style.dropdown}>
-            <span>
+          <li className={`${style.dropdown} ${style.categoryList}`}>
+            <span className={style.category}>
               <img src={wheelers} alt="two wheelers" />
               <div>Two Wheelers &nbsp;<img className={style.icon} src={arrow} alt="arrow"/></div>
             </span>
