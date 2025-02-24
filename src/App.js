9ec054e781c8cd0c5a77eff08d9a9eca72826d6d
Deepesh <vermadeepesh248@gmail.com>
2025-02-24 20:28:49 +0530

    more products added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Navbar from "./components/Navbar";
 import Category from "./components/Category";
 import SlidingImage from "./components/SlidingImage";
 import ProductContainer from "./components/ProductsContainer";
+import ProductTwo from "./components/ProductTwo";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Category />
       <SlidingImage />
-      <ProductContainer/>
+      <ProductContainer />
+      <ProductTwo/>
     </div>
   );
 }
